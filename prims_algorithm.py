ee588bc0b3e97cbe18ec19559e666200a8ad7623
Ha Hoang <hahoanglc97@gmail.com>
2021-12-23 11:07:49 +0700

    add test case for prims algorithm

--- prims_algorithm.py
+++ prims_algorithm.py
@@ -1,44 +1,57 @@
-class node:
+class nodeX:
     def __init__(self, name):
         self.name = name
         self.connections = {}
 
 
-n, m = list(map(int, input().strip().split()))
+def prims(n, edges, start):
+    graph = {}
+    for i in edges:
+        x, y, r = i
+        if x not in graph:
+            graph[x] = nodeX(x)
+        if y not in graph:
+            graph[y] = nodeX(y)
 
-# Build graph
-graph = {}
-for i in range(m):
-    x, y, r = list(map(int, input().strip().split()))
+        graph[x].connections[graph[y]] = r
+        graph[y].connections[graph[x]] = r
+    visited = {graph[start]: 0}
+    while len(visited) != n:
+        lowestCost = (None, float('inf'))
+        for node in visited:
+            for nextNode, weight in node.connections.items():
+                if nextNode not in visited and weight < lowestCost[1]:
+                    lowestCost = (nextNode, weight)
+        node, weight = lowestCost
+        visited[node] = weight
+    return sum(visited.values())
 
-    # Add the two nodes if they're not already in the graph
-    if x not in graph:
-        graph[x] = node(x)
-    if y not in graph:
-        graph[y] = node(y)
+if __name__ == '__main__':
+    fptr = open('test_case/primsmstsub-testcases.txt', 'r')
+    count = 0
+    edges = []
+    n = 0
+    m = 0
+    start = 0
+    result = 0
+    while True:
+        count += 1
 
-    # Connect the two nodes
-    graph[x].connections[graph[y]] = r
-    graph[y].connections[graph[x]] = r
+        # Get next line from file
+        line = fptr.readline()
 
-# Add starting node to visited list
-s = int(input().strip())
-visited = {graph[s]: 0}
+        # if line is empty
+        # end of file is reached
+        if not line:
+            break
 
-# Perform Prim's algorithm on graph
-while len(visited) != n:
-    lowestCost = (None, float('inf'))
-    for node in visited:
-        for nextNode, weight in node.connections.items():
-            if nextNode not in visited and weight < lowestCost[1]:
-                lowestCost = (nextNode, weight)
+        if count == 1:
+            n, k, start, result = list(map(int, line.rstrip().split()))
+        else:
+            contentLine = list(map(int, line.rstrip().split()))
+            edges.append(contentLine)
 
-    node, weight = lowestCost
-    visited[node] = weight
+    print('{:15} : {}'.format('Output', prims(n, edges, start)))
+    print('{:15} : {}'.format('Expected Output', result))
 
-# Print out sum of weights (aka cost of the MST)
-print(sum(visited.values()))
-
-# edges = [[1, 2, 3], [1, 3, 4], [4, 2, 6], [5, 2, 2], [2, 3, 5], [3, 5, 7]]
-# result = prims(5, edges, 1)
-# print(result)
+    fptr.close()
